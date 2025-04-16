640696f7b7194e843248487cc294aa92cc7ca1ab
ukanyarat <150550152+ukanyarat@users.noreply.github.com>
2025-04-17 05:50:00 +0700

    Update detail.tsx

--- frontend/app/(tabs)/detail.tsx
+++ frontend/app/(tabs)/detail.tsx
@@ -2,25 +2,28 @@ import { Text, View, ScrollView, TouchableOpacity } from "react-native";
 import HeaderCustom from "../components/header";
 import PaginationCustom from "../components/pagination";
 import { ItemCardMain } from "../components/itemCard";
-import {
-    ChevronLeft,
-    ChevronRight,
-    PlusCircle,
-    TrendingUp,
-    CreditCard,
-    DollarSign,
-    ArrowDownCircle,
-    ArrowUpCircle,
-    PieChart,
-    ArrowLeft,
-} from "lucide-react-native";
+import { ChevronLeft, ChevronRight, PlusCircle, TrendingUp, CreditCard, DollarSign, ArrowDownCircle, ArrowUpCircle, PieChart, ArrowLeft, } from "lucide-react-native";
+import { useState } from 'react';
+import { router } from "expo-router";
+import { ButtonMain } from "../components/button";
+
 export default function detail() {
 
+    const [page, setPage] = useState(1);
+    const totalPages = 4;
+
+    const handlePrev = () => {
+        if (page > 1) setPage(page - 1);
+    };
+
+    const handleNext = () => {
+        if (page < totalPages) setPage(page + 1);
+    };
+
     return (
-        <>
+        <View style={{ flex: 1 }}>
             <HeaderCustom title={`รายละเอียดการใช้จ่าย\nวันที่ 17/4/68`} />
-            <View className="p-4">
-                <View className="flex-row justify-between bg-white p-4 rounded-xl">
+            <View className="flex-row justify-between bg-blue-100 p-4 ">
                     <Text >ยอดใช้จ่ายวันที่......</Text>
                     <Text >฿ กี่บาทท</Text>
 
@@ -28,32 +31,30 @@ export default function detail() {
                     {/* {maxExpenseDay && ( */}
                     {/* )} */}
                 </View>
-                <Text className="text-base mb-4">แสดงรายการทั้งหมดของเดือนนั้นตาม created_at แค่เดือน...เท่านั้น </Text>
-                <ScrollView contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 30 }}>
-                    {/* <ButtonMain title="เพิ่มรายการใหม่" btnSize="full" className="mb-4" onPress={() => router.push('/new')} /> */}
-                    {Array.from({ length: 4 }).map((_, index) => (
-                        <ItemCardMain
-                            key={index}
-                            amount={10}
-                            typeTranfer="โอนเงิน"
-                            date="2023-08-01"
-                            time="10:00"
-                            sender="John Doe"
-                            recipient="Jane Smith"
-                            detail="รายละเอียดเพิ่มเติม"
-                            titleBtn="ลบ"
-                            btnColor="danger"
-                            onPress={() => { }}
-                        />
-                    ))}
-                </ScrollView>
-                <PaginationCustom
-                    page={1}
-                    totalPages={4}
-                    handlePrev={() => { }}
-                    handleNext={() => { }}
-                />
-            </View>
-        </>
-    )
+                <Text className="text-base mb-4">แสดงรายการทั้งหมดของเดือนนั้นตาม created_at วันเดียว...เท่านั้น </Text>
+            <ScrollView contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 30 }}>
+                {Array.from({ length: 4 }).map((_, index) => (
+                    <ItemCardMain
+                        key={index}
+                        amount={10}
+                        typeTranfer="โอนเงิน"
+                        date="2023-08-01"
+                        time="10:00"
+                        sender="John Doe"
+                        recipient="Jane Smith"
+                        detail="รายละเอียดเพิ่มเติม"
+                        titleBtn="ลบ"
+                        btnColor="danger"
+                        onPress={() => { }}
+                    />
+                ))}
+            </ScrollView>
+            <PaginationCustom
+                page={page}
+                totalPages={totalPages}
+                handlePrev={handlePrev}
+                handleNext={handleNext}
+            />
+        </View>
+    );
 }
