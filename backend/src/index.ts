7592d1359df61bb09a0994c70f221e935d22b500
Markeihoo <134832418+Markeihoo@users.noreply.github.com>
2025-04-16 08:14:51 +0700

    get backend

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import tranferRouter from "./router/tranfer-router";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", tranferRouter);
+
+app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`));
