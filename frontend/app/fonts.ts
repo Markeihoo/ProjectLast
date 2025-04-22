d40d534e26a3ccf4f487f959a0445cc66c5a11d4
Markeihoo <134832418+Markeihoo@users.noreply.github.com>
2025-04-22 14:58:10 +0700

    finist

---
+++ frontend/app/fonts.ts
@@ -0,0 +1,26 @@
+// // fonts.ts
+// import * as Font from 'expo-font';
+
+// export async function loadFonts() {
+//   await Font.loadAsync({
+//     AntDesign: require('./assets/fonts/AntDesign.ttf'),
+//     Entypo: require('./assets/fonts/Entypo.ttf'),
+//     EvilIcons: require('./assets/fonts/Evillcons.ttf'),
+//     Feather: require('./assets/fonts/Feather.ttf'),
+//     FontAwesome: require('./assets/fonts/FontAwesome.ttf'),
+//     FontAwesome5_Brands: require('./assets/fonts/FontAwesome5_Brands.ttf'),
+//     FontAwesome5_Regular: require('./assets/fonts/FontAwesome5_Regular.ttf'),
+//     FontAwesome5_Solid: require('./assets/fonts/FontAwesome5_Solid.ttf'),
+//     FontAwesome6_Brands: require('./assets/fonts/FontAwesome6_Brands.ttf'),
+//     FontAwesome6_Regular: require('./assets/fonts/FontAwesome6_Regular.ttf'),
+//     FontAwesome6_Solid: require('./assets/fonts/FontAwesome6_Solid.ttf'),
+//     Fontisto: require('./assets/fonts/Fontisto.ttf'),
+//     Foundation: require('./assets/fonts/Foundation.ttf'),
+//     Ionicons: require('./assets/fonts/lonicons.ttf'),
+//     MaterialCommunityIcons: require('./assets/fonts/MaterialCommunitylcons.ttf'),
+//     MaterialIcons: require('./assets/fonts/Materiallcons.ttf'),
+//     Octicons: require('./assets/fonts/Octicons.ttf'),
+//     SimpleLineIcons: require('./assets/fonts/SimpleLinelcons.ttf'),
+//     Zocial: require('./assets/fonts/Zocial.ttf'),
+//   });
+// }
--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -2,4 +2,12 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
-module.exports = withNativeWind(config, { input: './app/global.css' });
+
+// รองรับนามสกุล .svg ให้ทำงานผ่าน react-native-svg
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
+config.resolver.sourceExts.push("svg");
+
+// ถ้าจะใช้ svg แบบ import เป็น component (เช่น import Icon from './icon.svg')
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+
+module.exports = withNativeWind(config, { input: "./app/global.css" });
